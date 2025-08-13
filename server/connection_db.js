8d09516222c42f056ab24f5e2536eb6d76d50f1e
giselle-01 <giselleandrea027@gmail.com>
2025-08-13 17:28:39 -0500

    Fix: The connection to the database was created.

---
+++ server/connection_db.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2/promise';
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'admin',
+    port: '3306',
+    password: 'Qwe.123*',
+    database: 'expert_soft'
+});
+
+async function testDbConnection() {
+    try {
+        const connection = await pool.getConnection();
+        console.log('Successful connection to the database');
+        connection.release();
+    } catch (error) {
+        console.error('Error connecting to the database:', error.message);
+    }
+}
+
+testDbConnection()
